4b05995101fb61f101a3160bd6e5edc89b51117a
ruvelbarua <ruvel.br78@gmail.com>
2021-11-17 21:59:55 +0600

    update

--- src/Pages/Home/Users/Users.js
+++ src/Pages/Home/Users/Users.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Booking from '../../Products/Booking/Booking';
+import ManageProducts from '../../Products/ManageProducts/ManageProducts';
 import ProductBanner from '../../Products/ProductBanner/ProductBanner';
 import ProductOrder from '../../Products/ProductOrder/ProductOrder';
 
@@ -7,10 +8,10 @@ const Users = () => {
     const [date, setDate] = useState(new Date());
     return (
         <div>
-            <ProductOrder></ProductOrder>
             <ProductBanner date={date} setDate={setDate} />
             <Booking date={date} />
-
+            <ProductOrder></ProductOrder>
+            <ManageProducts></ManageProducts>
         </div>
     );
 };
