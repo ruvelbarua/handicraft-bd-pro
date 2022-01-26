777cc86aba93343a653e795bc8f52a130eef25b7
ruvelbarua <ruvel.br78@gmail.com>
2022-01-26 21:11:56 +0600

    update

--- src/Pages/Dashboard/MakeAdmin/MakeAdmin.js
+++ src/Pages/Dashboard/MakeAdmin/MakeAdmin.js
@@ -1,38 +1,43 @@
 import React, { useState } from 'react';
+import { TextField, Button } from '@mui/material';
 import useAuth from '../../../contexts/AuthProvider/useAuth';
 
 const MakeAdmin = () => {
     const [email, setEmail] = useState('');
-    const [success, setSuccess] = useState(false);
-    const { token } = useAuth();
-
-    const handleOnBlur = e => {
+    const handleonBlur = e => {
         setEmail(e.target.value);
     }
+    // const [success, setSuccess] = useState(false);
+    // const { token } = useAuth();
+
 
     const handleAdminSubmit = e => {
         const user = { email };
-        fetch('', {
+        fetch('http://localhost:5000/users/admin', {
             method: 'PUT',
             headers: {
-                'authorization': `Bearer ${token}`,
+                // 'authorization': `Bearer ${token}`,
                 'content-type': 'application/json'
             },
             body: JSON.stringify(user)
         })
             .then(res => res.json())
             .then(data => {
-                if (data.modifiedCount) {
-                    console.log(data);
-                    setSuccess(true);
-                }
-
+                console.log(data);
             })
         e.preventDefault()
     }
     return (
         <div>
             <h2>This is Make Admin Page</h2>
+            <form onSubmit={handleAdminSubmit}>
+                <TextField
+                    label="Email"
+                    type="email"
+                    onBlur={handleonBlur}
+                    variant="standard" />
+                <Button type="submit" variant="contained">Make Admin</Button>
+            </form>
         </div>
     );
 };
